072f2f508a6182752194590934caa7878ea802b2
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-19 23:14:26 +0100

    Update start.py

--- start.py
+++ start.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from keep_alive import keep_alive
 import random
+import json
 import asyncio
 import pymongo
 from pymongo import MongoClient
